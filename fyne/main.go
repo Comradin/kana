b93b43481eb7733cb9a7641a82a07831efc0bc08
Marcus Franke <marcus@comradin.dev>
2026-04-14 17:04:06 +0200

    feat(fyne): wire app, game-over dialog, event watcher

--- fyne/main.go
+++ fyne/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"fyne.io/fyne/v2/app"
+	"kana/store"
 )
 
 func main() {
+	st, err := store.Open("kana.db")
+	if err != nil {
+		fmt.Printf("Error opening store: %v\n", err)
+		os.Exit(1)
+	}
+	defer st.Close()
+
 	a := app.New()
-	w := a.NewWindow("Kana")
+	w := buildWindow(a, st)
 	w.ShowAndRun()
 }
